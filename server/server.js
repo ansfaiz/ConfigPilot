ac024aa6a4e52805fb0a67e959a0a58b76874358
md faiyaz ansari <mdfaiyaza130@gmail.com>
2026-04-28 23:23:30 +0530

    update claude db

--- server/server.js
+++ server/server.js
@@ -8,7 +8,8 @@ const db = require('./config/db');
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173",
+  "http://localhost:3000"], credentials: true }));
 app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/records', dataRoutes);
